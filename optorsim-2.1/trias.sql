6d5ed859614ab44379928ccc92289375d5d19a3b
Ahmed Hosni <ahhosni@VISTAPRINT.NET>
2016-10-05 18:52:21 +0100

    Update schema

--- optorsim-2.1/trias.sql
+++ optorsim-2.1/trias.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 05 Octobre 2016 à 00:17
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Généré le :  Mer 05 Octobre 2016 à 19:51
+-- Version du serveur :  10.1.16-MariaDB
+-- Version de PHP :  7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `trias`
@@ -26,18 +26,16 @@ SET time_zone = "+00:00";
 -- Structure de la table `bgrt`
 --
 
-CREATE TABLE IF NOT EXISTS `bgrt` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `bgrt` (
+  `id` int(11) NOT NULL,
   `premisse` varchar(512) NOT NULL,
   `conclusion` varchar(512) NOT NULL,
   `supp_c` float NOT NULL,
   `conf_c` float NOT NULL,
   `tasks_count` int(11) NOT NULL,
   `sites_count` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
+  `rt_sites` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -45,12 +43,10 @@ CREATE TABLE IF NOT EXISTS `bgrt` (
 -- Structure de la table `gridfiles`
 --
 
-CREATE TABLE IF NOT EXISTS `gridfiles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `label` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `label` (`label`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `gridfiles` (
+  `id` int(11) NOT NULL,
+  `label` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -58,12 +54,10 @@ CREATE TABLE IF NOT EXISTS `gridfiles` (
 -- Structure de la table `gridsites`
 --
 
-CREATE TABLE IF NOT EXISTS `gridsites` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `label` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `label` (`label`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `gridsites` (
+  `id` int(11) NOT NULL,
+  `label` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -71,12 +65,10 @@ CREATE TABLE IF NOT EXISTS `gridsites` (
 -- Structure de la table `gridtasks`
 --
 
-CREATE TABLE IF NOT EXISTS `gridtasks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `label` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `label` (`label`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `gridtasks` (
+  `id` int(11) NOT NULL,
+  `label` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -84,14 +76,80 @@ CREATE TABLE IF NOT EXISTS `gridtasks` (
 -- Structure de la table `triconcepts`
 --
 
-CREATE TABLE IF NOT EXISTS `triconcepts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `triconcepts` (
+  `id` int(11) NOT NULL,
   `id_task` int(11) NOT NULL,
   `id_file` int(11) NOT NULL,
-  `id_site` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id_site` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `bgrt`
+--
+ALTER TABLE `bgrt`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_index` (`premisse`,`conclusion`);
+
+--
+-- Index pour la table `gridfiles`
+--
+ALTER TABLE `gridfiles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `label` (`label`) USING BTREE;
+
+--
+-- Index pour la table `gridsites`
+--
+ALTER TABLE `gridsites`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `label` (`label`);
+
+--
+-- Index pour la table `gridtasks`
+--
+ALTER TABLE `gridtasks`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `label` (`label`);
+
+--
+-- Index pour la table `triconcepts`
+--
+ALTER TABLE `triconcepts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `bgrt`
+--
+ALTER TABLE `bgrt`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `gridfiles`
+--
+ALTER TABLE `gridfiles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `gridsites`
+--
+ALTER TABLE `gridsites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `gridtasks`
+--
+ALTER TABLE `gridtasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `triconcepts`
+--
+ALTER TABLE `triconcepts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
