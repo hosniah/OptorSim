10f9c556d8d64dd6b1a2597e29c720d0ff72b35d
Ahmed Hosni <ahmedhosni.contact@gmail.com>
2016-10-23 18:38:32 +0100

    Fix sites name selection instead of sites id to retrieve RAT

--- optorsim-2.1/src/org/edg/data/replication/optorsim/optor/TriARModel.java
+++ optorsim-2.1/src/org/edg/data/replication/optorsim/optor/TriARModel.java
@@ -15,8 +15,10 @@ public class TriARModel {
     public float bestConfidence;
     public List<Integer> RTrep;
     public String[] File_To_replicate;
+   public String[] RATSITENAMES;
     public MySQLAccess dao;
     public int site_id;
+    public String current_site;
     public List<Integer> all_site_ids;
 
     public TriARModel(GridSite site) {
@@ -24,7 +26,8 @@ public class TriARModel {
         this.bestConfidence = 0;
         RTrep        = new ArrayList<Integer>();
         all_site_ids = new ArrayList<Integer>();
-        this.site_id = getGridSiteIdByname(site.toString());
+        this.site_id = 0;//getGridSiteIdByname(site.toString());
+        this.current_site    = site.toString();
        // System.out.println("current optimiser site is: "+this.site_id);
     }
 
@@ -43,7 +46,23 @@ public class TriARModel {
         return site_id;
     }
      
+    private String getGridSiteNameById(String site_id) {
+        String site_label = new String();
+        ResultSet res;
+        try {
+            res = this.dao.query("Select * from allvisitedsites where id = '"+site_id+"';");
+            while(res.next()) {
+                site_label       =  (String) res.getObject("label"); 
+                //this.all_site_ids.add(site_id);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+        return site_label;
+    }
+    
     private boolean isCurrentSiteAmongTriadicRuleSites(List<String> elephantList) {
+       /*
         Iterator<Integer> it = all_site_ids.iterator();
         while(it.hasNext()) {
             Object obj = it.next();
@@ -52,7 +71,28 @@ public class TriARModel {
                 return true;
             }
         }
+        */
         return false;
+    }
+    
+    public boolean TARSitesContainsCurrentSite_byName(List<String> elephantList) {
+        if (elephantList.contains(this.current_site)) {
+            return true;
+        } else {
+            return false;
+        }        
+    }
+    
+    public String[] getRATStiesNameasListFromIds(List<String> idsList) {
+        List<String> sitesNameas = new ArrayList<String>();
+        for (int i = 0; i < idsList.size(); i++)
+        {
+            
+            String sitename = getGridSiteNameById(idsList.get(i));
+            sitesNameas.add(sitename);
+        }
+        String[] stringArray = sitesNameas.toArray(new String[0]);
+        return stringArray;
     }
     
     /** Query the tabe trias.bgrt with ordering on tasks_count, sites_count to prepare RAT set.
@@ -71,10 +111,15 @@ public class TriARModel {
                 String RT_sites     = (String) res.getObject("rt_sites");                
                 // Is the current site among RT group of sites
                 List<String> elephantList = Arrays.asList(RT_sites.split(","));     
-                System.out.println("************ "+this.site_id+" current RT sites are: "+RT_sites);
-               // if (elephantList.contains(this.site_id)) {
-                if (isCurrentSiteAmongTriadicRuleSites(elephantList) != true) {
-                     System.out.println("Skipping RT: "+RT_sites+". The site #"+this.site_id+" is not among its group of grid sites.");
+               // System.out.println("************ "+this.site_id+" current RT sites are: "+RT_sites);
+                // if (elephantList.contains(this.site_id)) {
+                // if (isCurrentSiteAmongTriadicRuleSites(elephantList) != true) {
+                
+                String[] RT_sitesNames = this.getRATStiesNameasListFromIds(elephantList);
+                List<String> siteNamesList = Arrays.asList(RT_sitesNames); 
+                if (TARSitesContainsCurrentSite_byName(siteNamesList) != true) {
+                    System.out.println("Skipping RT: "+RT_sites);
+                   //  System.out.println("Skipping RT: "+RT_sites+". The site #"+this.site_id+" is not among its group of grid sites.");
                 } else {
                     // Here comes the issue
                     System.out.println("current RT sites are: "+RT_sites);
